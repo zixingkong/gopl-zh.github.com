d7ddbc8c6576e8efecac9879bbafd89cc0091850
harveyywang <harveyywang@tencent.com>
2024-07-29 19:57:49 +0800

    feat: 第4章 习题4.8

---
+++ ch4/exercise/ex_4.8.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+//练习 4.8： 修改charcount程序，使用unicode.IsLetter等相关的函数，统计字母、数字等Unicode中不同的字符类别。
+// Charcount computes counts of Unicode characters.
+
+func whatType(r rune) string {
+	if unicode.IsNumber(r) {
+		return "number"
+	}
+	if unicode.Is(unicode.Han, r) {
+		return "中文"
+	}
+	if r >= 65 && r <= 122 {
+		return "英文"
+	}
+	return "any"
+}
+
+func main() {
+	counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	hash := make(map[string]int, 4)
+	invalid := 0 // count of invalid UTF-8 characters
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		t := whatType(r)
+		hash[t]++
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	fmt.Println("%#v", hash)
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
